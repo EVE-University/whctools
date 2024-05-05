6583a9c5f2ce33a0f5312292096b0047b9ef70b9
Anthony <lynkfox@gmail.com>
2024-05-05 15:10:24 -0400

    comment

--- whctools/signals.py
+++ whctools/signals.py
@@ -1,4 +1,2 @@
-# TODO
-
-# Need a Signal check on member state change to Alumni to remove from all ACLS
+# TODO @@@ Need a Signal check on member state change to Alumni to remove from all ACLS
 # could add it to group assigner?
